cd99db5bedc86824fdb0f6186654202c10a2b6d9
jinseok Bea <115687246+jimseokbea@users.noreply.github.com>
2025-10-27 23:17:48 +0900

    feat: 메인 실행 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String[] carNames = inputCarNames();
+            int attempts = inputAttempts();
+
+            RacingGame game = new RacingGame(carNames, attempts);
+            game.play();
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            throw e;
+        }
+    }
+
+    private static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return input.split(",");
+    }
+
+    private static int inputAttempts() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 }
